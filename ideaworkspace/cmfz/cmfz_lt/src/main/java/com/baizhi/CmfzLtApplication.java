26fe28ddb06d453a9d49cc843f7f8b62320cb226
lt <784119872@qq.com>
2019-07-24 17:41:04 +0800

    first init

--- ideaworkspace/cmfz/cmfz_lt/src/main/java/com/baizhi/CmfzLtApplication.java
+++ ideaworkspace/cmfz/cmfz_lt/src/main/java/com/baizhi/CmfzLtApplication.java
@@ -10,9 +10,6 @@ public class CmfzLtApplication {
 
     public static void main(String[] args) {
         System.out.println("第1次提交");
-        System.out.println("第2次提交");
-        System.out.println("第3次提交");
-        System.out.println("第4次提交");
 
         SpringApplication.run(CmfzLtApplication.class, args);
     }
