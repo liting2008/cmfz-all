99bed166193652117369d77db3dbe2257fc4fbd8
lt <784119872@qq.com>
2019-07-24 15:30:03 +0800

    Initial commit

---
+++ ideaworkspace/cache/src/main/java/com/baizhi/cache/CacheApplication.java
@@ -0,0 +1,15 @@
+package com.baizhi.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheApplication {
+
+    public static void main(String[] args) {
+        System.out.println("测试");
+        System.out.println("测试2");
+
+        SpringApplication.run(CacheApplication.class, args);
+    }
+}
