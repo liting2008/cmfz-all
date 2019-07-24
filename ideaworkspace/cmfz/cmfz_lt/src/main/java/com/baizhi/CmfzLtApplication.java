48607682ec7d5264e42b79e808f254c9ea1c942f
lt <784119872@qq.com>
2019-07-24 19:36:45 +0800

    这是master第4次提交

--- ideaworkspace/cmfz/cmfz_lt/src/main/java/com/baizhi/CmfzLtApplication.java
+++ ideaworkspace/cmfz/cmfz_lt/src/main/java/com/baizhi/CmfzLtApplication.java
@@ -12,7 +12,7 @@ public class CmfzLtApplication {
         System.out.println("第1次提交");
         System.out.println("第2次提交");
         System.out.println("第3次提交");
-        System.out.println("================");
+        System.out.println("==================");
 
         SpringApplication.run(CmfzLtApplication.class, args);
     }
