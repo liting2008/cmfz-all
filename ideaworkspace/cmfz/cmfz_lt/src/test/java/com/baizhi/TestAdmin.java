6b5c3f13d9148a6042e60161552365cbf8780c9c
lt <784119872@qq.com>
2019-07-27 00:04:33 +0800

    这是TestAdmin

---
+++ ideaworkspace/cmfz/cmfz_lt/src/test/java/com/baizhi/TestAdmin.java
@@ -0,0 +1,30 @@
+package com.baizhi;
+
+import com.baizhi.entity.Admin;
+import com.baizhi.service.AdminService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.UUID;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestAdmin {
+        @Autowired
+        private AdminService adminService;
+        @Test
+        public void test1(){
+            List<Admin> list = adminService.queryAll();
+            for (Admin admin : list) {
+                System.out.println(admin);
+            }
+        }
+        @Test
+    public void test2(){
+            adminService.addAdmin(new Admin(UUID.randomUUID().toString(),"123456","123456"));
+        }
+}
